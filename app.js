bbfc2c4eccbe6915d3585c700ac02c85bdf757b6
Yury Bursian <dab180996@gmail.com>
2024-07-25 14:40:05 +0300

    mongoose connect

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/news-explorer")
+  .then(() => {
+    console.log("Connected to the Database");
+  })
+  .catch(console.error);
